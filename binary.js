71f1505b568f0e9962fa60ae64e163c12271a7f4
Arthur Barski <barski.10877716@gmail.com>
2019-05-25 15:26:00 +0300

    Final commit
    Binary search is done.

--- binary.js
+++ binary.js
@@ -1,17 +1,40 @@
 // Бинарный поиск
 let array = [1,192,163,99,24,25,65,777,1,83, 12];
-
-function binarySearch(arr, value){
+//Выведем наш не сортированный массив
+console.log(array);
+// arr - наш массив элементов, value искомое число
+function binarySearch(arr, value) {
     let high = arr.length - 1; // Чтобы найти последний индекс массива
     let low = 0;
     let mid = 0;
     
     while (low <= high){
         mid = Math.floor((high + low) / 2); // Находим среднее значения индекса
+        // Если искомое значение имеет индекс = середине(mid), то выводим значение середины(mid)
         if (arr[mid] === value) {
             return arr[mid];
+        } else if (value > arr[mid]) {
+            // Если искомое значение выше середины(mid), то началом(low) становится mid + 1, тем самым лишняя
+            // половина убирается, то есть удаляются все цифры до середины(low)
+            low = mid + 1;
+        } else {
+            // Если ниже, то удаляются все цифры от середины
+            high = mid - 1;
         }
     }
+    // Если не найдет значение, то выведет "Нет такого числа"
+    return "Нет такого числа :(";
 }
-console.log(array);
+// Сортируем наш массив чисел, так как бинарный поиск только для сорт массива работает
+let sortedArray = array.sort(
+    function (a,b) {
+        return a-b;
+    }
+);
+// Выполним нашу функцию (тест)
+let foundNumber = binarySearch(sortedArray, 777);
+// Выведем наш сортированный массив
+console.log(sortedArray);
+// И результат поиска
+console.log(foundNumber);
 
